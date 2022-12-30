5d3d95ce17b60034ea9254bec3a775e1b9f5cc84
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-12-30 17:38:49 +0100

    Add missing Deconstruct methods for DateTime

--- CSharpUtilities/DateTimeExtension.cs
+++ CSharpUtilities/DateTimeExtension.cs
@@ -15,15 +15,25 @@ public static class DateTimeExtension
     public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hour, out int minute, out int second)
     {
         Deconstruct(dateTime, out year, out month, out day);
-        hour = dateTime.Hour;
-        minute = dateTime.Minute;
-        second = dateTime.Second;
+        TimeSpanExtension.Deconstruct(dateTime.TimeOfDay, out hour, out minute, out second);
     }
 
     public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hour, out int minute, out int second, out int millisecond)
     {
-        Deconstruct(dateTime, out year, out month, out day, out hour, out minute, out second);
-        millisecond = dateTime.Millisecond;
+        Deconstruct(dateTime, out year, out month, out day);
+        TimeSpanExtension.Deconstruct(dateTime.TimeOfDay, out hour, out minute, out second, out millisecond);
+    }
+
+    public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hour, out int minute, out int second, out int millisecond, out int microsecond)
+    {
+        Deconstruct(dateTime, out year, out month, out day);
+        TimeSpanExtension.Deconstruct(dateTime.TimeOfDay, out hour, out minute, out second, out millisecond, out microsecond);
+    }
+
+    public static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hour, out int minute, out int second, out int millisecond, out int microsecond, out int nanosecond)
+    {
+        Deconstruct(dateTime, out year, out month, out day);
+        TimeSpanExtension.Deconstruct(dateTime.TimeOfDay, out hour, out minute, out second, out millisecond, out microsecond, out nanosecond);
     }
 
     #endregion
