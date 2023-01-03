f32d7591a83f1f17a70b13ac505012f68192ea3c
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-01-03 17:42:14 +0100

    Remove Builder. Implement FpFuncBuilder.

---
+++ CSharpUtilities/FunctionalProgramming/FpFuncBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpUtilities.FunctionalProgramming;
+
+public static partial class Fp
+{
+
+    public static Func<T> SetupBuild<T>(this T next, Func<T, T> build)
+    {
+        return () => build(next);
+    }
+
+    public static Func<TTo> SetupBuild<TFrom, TTo>(this TFrom next, Func<TFrom, TTo> build)
+    {
+        return () => build(next);
+    }
+
+}
