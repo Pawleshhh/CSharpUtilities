19834d6db8b5ecab9949e6f9387d7d188618a984
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-01-03 21:51:37 +0100

    ArrayExtension: IndexOf, Clear

---
+++ CSharpUtilities/ArrayExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpUtilities;
+
+public static class ArrayExtension
+{
+
+    public static int IndexOf<T>(this T[] array, T value)
+        => Array.IndexOf(array, value);
+
+    public static void Clear<T>(this T[] array)
+        => Array.Clear(array);
+
+    public static void Clear<T>(this T[] array, int index, int length)
+       => Array.Clear(array, index, length);
+
+}
