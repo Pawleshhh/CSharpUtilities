86382f79b5fa1b23c8fd968b58a213224792aa77
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-02-17 19:07:34 +0100

    Add 'Try' prefix for StringExtension.IndexOf methods

--- CSharpUtilities/BasicTypes/StringExtension.cs
+++ CSharpUtilities/BasicTypes/StringExtension.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Runtime.CompilerServices;
-using static CSharpUtilities.UtilsHelper;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using static CSharpUtilities.UtilsHelper;
 
 namespace CSharpUtilities;
 
@@ -12,55 +8,55 @@ public static class StringExtension
     public static bool TryIndexOf(this string @string, string value, out int index)
         => TryGetIndexHelper(() => @string.IndexOf(value), out index);
 
-    public static bool IndexOf(this string @string, string value, int startIndex, out int index)
+    public static bool TryIndexOf(this string @string, string value, int startIndex, out int index)
         => TryGetIndexHelper(() => @string.IndexOf(value, startIndex), out index);
 
-    public static bool IndexOf(this string @string, string value, int startIndex, int count, out int index)
+    public static bool TryIndexOf(this string @string, string value, int startIndex, int count, out int index)
         => TryGetIndexHelper(() => @string.IndexOf(value, startIndex, count), out index);
 
-    public static bool IndexOf(this string @string, string value, StringComparison comparisonType, out int index)
+    public static bool TryIndexOf(this string @string, string value, StringComparison comparisonType, out int index)
         => TryGetIndexHelper(() => @string.IndexOf(value, comparisonType), out index);
 
-    public static bool IndexOf(this string @string, string value, int startIndex, StringComparison comparisonType, out int index)
+    public static bool TryIndexOf(this string @string, string value, int startIndex, StringComparison comparisonType, out int index)
         => TryGetIndexHelper(() => @string.IndexOf(value, startIndex, comparisonType), out index);
 
-    public static bool IndexOf(this string @string, string value, int startIndex, int count, StringComparison comparisonType, out int index)
+    public static bool TryIndexOf(this string @string, string value, int startIndex, int count, StringComparison comparisonType, out int index)
         => TryGetIndexHelper(() => @string.IndexOf(value, startIndex, count, comparisonType), out index);
 
-    public static bool LastIndexOf(this string @string, char value, out int index)
+    public static bool TryLastIndexOf(this string @string, char value, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value), out index);
 
-    public static bool LastIndexOf(this string @string, char value, int startIndex, out int index)
+    public static bool TryLastIndexOf(this string @string, char value, int startIndex, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, startIndex), out index);
 
-    public static bool LastIndexOf(this string @string, char value, int startIndex, int count, out int index)
+    public static bool TryLastIndexOf(this string @string, char value, int startIndex, int count, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, startIndex, count), out index);
 
-    public static bool LastIndexOfAny(this string @string, char[] anyOf, out int index)
+    public static bool TryLastIndexOfAny(this string @string, char[] anyOf, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOfAny(anyOf), out index);
 
-    public static bool LastIndexOfAny(this string @string, char[] anyOf, int startIndex, out int index)
+    public static bool TryLastIndexOfAny(this string @string, char[] anyOf, int startIndex, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOfAny(anyOf, startIndex), out index);
 
-    public static bool LastIndexOfAny(this string @string, char[] anyOf, int startIndex, int count, out int index)
+    public static bool TryLastIndexOfAny(this string @string, char[] anyOf, int startIndex, int count, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOfAny(anyOf, startIndex, count), out index);
 
-    public static bool LastIndexOf(this string @string, string value, out int index)
+    public static bool TryLastIndexOf(this string @string, string value, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value), out index);
 
-    public static bool LastIndexOf(this string @string, string value, int startIndex, out int index)
+    public static bool TryLastIndexOf(this string @string, string value, int startIndex, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, startIndex), out index);
 
-    public static bool LastIndexOf(this string @string, string value, int startIndex, int count, out int index)
+    public static bool TryLastIndexOf(this string @string, string value, int startIndex, int count, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, startIndex, count), out index);
 
-    public static bool LastIndexOf(this string @string, string value, StringComparison comparisonType, out int index)
+    public static bool TryLastIndexOf(this string @string, string value, StringComparison comparisonType, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, comparisonType), out index);
 
-    public static bool LastIndexOf(this string @string, string value, int startIndex, StringComparison comparisonType, out int index)
+    public static bool TryLastIndexOf(this string @string, string value, int startIndex, StringComparison comparisonType, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, startIndex, comparisonType), out index);
 
-    public static bool LastIndexOf(this string @string, string value, int startIndex, int count, StringComparison comparisonType, out int index)
+    public static bool TryLastIndexOf(this string @string, string value, int startIndex, int count, StringComparison comparisonType, out int index)
         => TryGetIndexHelper(() => @string.LastIndexOf(value, startIndex, count, comparisonType), out index);
 
     public static bool IsNullOrEmpty(this string? @string)
